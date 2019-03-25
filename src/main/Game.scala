359a76c1e341fa544694fad00dd0a61693db4209
lukem <lukemeil@buffalo.edu>
2019-03-25 15:42:19 -0400

    Luke - Deciding API

--- src/main/Game.scala
+++ src/main/Game.scala
@@ -10,8 +10,13 @@ class Game(player: Player){
     player.velocity.x = direction.x * speed
     player.velocity.y = direction.y * speed
     player.get_orientation()
-
   }
+
+  abstract def playerMove()
+
+  abstract def jump()
+
+  abstract def shoot()
 
 
 }
