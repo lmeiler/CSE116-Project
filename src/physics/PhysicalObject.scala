2ade2772194e4e48b7ee9fca4045a6dc974052db
lukem <lukemeil@buffalo.edu>
2019-03-28 14:07:10 -0400

    Luke - Adding Physics Object

--- src/physics/PhysicalObject.scala
+++ src/physics/PhysicalObject.scala
@@ -1,5 +1,105 @@
 package physics
 
-abstract class PhysicalObject(location: PhysicsVector, velocity: PhysicsVector) {
+import java.lang.Math.{max, min}
 
+import physics.Physics.computePotentialLocation
+
+abstract class PhysicalObject(var location: PhysicsVector, var velocity: PhysicsVector) {
+
+  def computePotentialLocation(deltaTime: Double): PhysicsVector = {
+    var velocityVector: PhysicsVector = this.velocity
+    var locationVector: PhysicsVector = this.location
+    var velocityX: Double = velocityVector.x
+    var velocityY: Double = velocityVector.y
+    var velocityZ: Double = velocityVector.z
+    var locationX: Double = locationVector.x
+    var locationY: Double = locationVector.y
+    var locationZ: Double = locationVector.z
+    var xDistanceTraveled: Double = velocityX * deltaTime
+    var yDistanceTraveled: Double = velocityY * deltaTime
+    var zDistanceTraveled: Double = velocityZ * deltaTime
+    var newXLocation: Double = locationX + xDistanceTraveled
+    var newYLocation: Double = locationY + yDistanceTraveled
+    var newZLocation: Double = (locationZ + zDistanceTraveled)
+    if (newZLocation <= 0) {
+      var groundVector: PhysicsVector = new PhysicsVector(newXLocation, newYLocation, 0.0)
+      return groundVector
+    }
+    else {
+      var newVector: PhysicsVector = new PhysicsVector(newXLocation, newYLocation, newZLocation)
+      return newVector
+
+    }
+  }
+
+  def updateVelocity(world: World, deltaTime: Double): Unit = {
+    var objectVelocity: PhysicsVector = this.velocity
+    var objectZVelocity: Double = objectVelocity.z
+    var gravity: Double = world.gravity
+    var deltaVelocity: Double = gravity*deltaTime
+    var finalVelocity: Double = (objectZVelocity - deltaVelocity)
+    //    var halfATSquared: Double = (1/2)*(gravity)*(deltaTime)*(deltaTime)
+    var vNotT: Double = objectZVelocity*deltaTime
+    //    var newZ: Double = vNotT - halfATSquared
+    var newZ: Double = this.location.z + vNotT
+    var newVelocity: PhysicsVector = new PhysicsVector(this.velocity.x, this.velocity.y, finalVelocity)
+    var newNonZVelocity: PhysicsVector = new PhysicsVector(this.velocity.x, this.velocity.y, 0.0)
+    if (this.location.z == 0.0 && this.velocity.z < 0.0) {
+      this.velocity = newNonZVelocity
+    }
+    else {
+      this.velocity = newVelocity
+    }
+  }
+
+  def detectCollision(location3: PhysicsVector, boundary3: Boundary): Boolean = {
+    var boundEnd1: PhysicsVector = boundary3.end1
+    var boundEnd2: PhysicsVector = boundary3.end2
+    var boundEnd1X: Double = boundEnd1.x
+    var boundEnd1Y: Double = boundEnd1.y
+    var boundEnd2X: Double = boundEnd2.x
+    var boundEnd2Y: Double = boundEnd2.y
+    var objx: Double = this.location.x
+    var objy: Double = this.location.y
+    var locx: Double = location3.x
+    var locy: Double = location3.y
+    var objectSlope: Double = (locy - objy)/(locx - objx)
+    var boundSlope: Double = (boundEnd2Y - boundEnd1Y)/(boundEnd2X - boundEnd1X)
+    var objYInt: Double = (objy - (objectSlope)*objx)
+    var boundYInt: Double = (boundEnd1Y - (boundSlope)*boundEnd1X)
+    var xIntercept: Double = (boundYInt - objYInt)/(objectSlope - boundSlope)
+    var minX1: Double = min(objx, locx)
+    var minX2: Double = min(boundEnd1X, boundEnd2X)
+    var maxX1: Double = max(objx, locx)
+    var maxX2: Double = max(boundEnd1X, boundEnd2X)
+    if (minX1 <= xIntercept && xIntercept <= maxX1) {
+      if (minX2 <= xIntercept && xIntercept <= maxX2) {
+        return false
+      }
+      else {
+        return true
+      }
+    }
+    else {
+      return true
+    }
+  }
+
+//  def updateWorld(world: World, deltaTime: Double): Unit = {
+//    for (obj <- world.objects) {
+//      updateVelocity(obj, world, deltaTime)
+//      var collision: Boolean = false
+//      for (bound <- world.boundaries) {
+//        if (!detectCollision(obj, computePotentialLocation(obj, deltaTime), bound)) {
+//          collision = true
+//        }
+//      }
+//      if (collision == true) {
+//        obj.location.z = computePotentialLocation(obj, deltaTime).z
+//      }
+//      else {
+//        obj.location = computePotentialLocation(obj, deltaTime)
+//      }
+//    }
+//  }
 }
