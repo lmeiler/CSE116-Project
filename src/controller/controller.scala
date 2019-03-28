43f66d8ed5c7a4240a1dad6a41d1f64bca6fac4a
Jimmy <holtzmanjimmy@yahoo.com>
2019-03-28 14:47:30 -0400

    added actions

---
+++ src/controller/controller.scala
@@ -0,0 +1,31 @@
+package controller
+
+  import javafx.event.{ActionEvent, EventHandler}
+  import main._
+//each class is a different key press
+class pressW(game: Game) extends EventHandler[ActionEvent] {
+  override def handle(event: ActionEvent): Unit = {
+
+  }
+}
+class pressS(game: Game) extends EventHandler[ActionEvent] {
+  override def handle(event: ActionEvent): Unit = {
+
+  }
+}
+class pressA(game: Game) extends EventHandler[ActionEvent] {
+  override def handle(event: ActionEvent): Unit = {
+
+  }
+}
+class pressD(game: Game) extends EventHandler[ActionEvent] {
+  override def handle(event: ActionEvent): Unit = {
+
+  }
+}
+class pressV(game: Game) extends EventHandler[ActionEvent] {
+  override def handle(event: ActionEvent): Unit = {
+
+  }
+
+}
