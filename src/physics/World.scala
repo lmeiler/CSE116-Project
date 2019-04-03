f196c919ff9ed8f2913fa0b7102d0384ced4cdc0
lukem <lukemeil@buffalo.edu>
2019-04-02 20:19:24 -0400

    Luke - Single Player Update

--- src/physics/World.scala
+++ src/physics/World.scala
@@ -10,28 +10,29 @@ import scala.collection.mutable.ListBuffer
 
 class World(val boundariesSet: List[Boundary]) {
 
-//  var players: mutable.MutableList[Player] = mutable.MutableList()
+  //  var players: mutable.MutableList[Player] = mutable.MutableList()
 
   var projectiles: mutable.Buffer[Projectile] = mutable.Buffer.empty
-  val gravity : Double = 9.8
-//  val boundariesSet: List[Boundary] = List.empty
+  val gravity: Double = 9.8
+  //  val boundariesSet: List[Boundary] = List.empty
   var lastUpdateTime: Double = System.nanoTime()
   var players: mutable.ListBuffer[Player] = mutable.ListBuffer[Player]()
+  var player: Player = new Player(new PhysicsVector(1, 0), new PhysicsVector(0, 0), "user", this)
 
   def eliminatePlayers(): Unit = {
-//    val buffer: mutable.Buffer[Player] = this.players.toBuffer
-//    val list1: List[Player] = this.players
+    //    val buffer: mutable.Buffer[Player] = this.players.toBuffer
+    //    val list1: List[Player] = this.players
     var index: Int = 0
-//    var buffer: mutable.Buffer[Player] = mutable.Buffer()
+    //    var buffer: mutable.Buffer[Player] = mutable.Buffer()
     for (player <- this.players) {
       if (player.health != 0) {
-//        buffer.remove(index)
+        //        buffer.remove(index)
         this.players -= player
       }
       index += 1
     }
-//    val newList: List[Player] = buffer.toList
-//    this.players = newList
+    //    val newList: List[Player] = buffer.toList
+    //    this.players = newList
   }
 
   def checkWinner(): String = {
@@ -40,7 +41,7 @@ class World(val boundariesSet: List[Boundary]) {
       val playerName: String = onlyPlayer.username
       playerName + " has won!!"
     }
-    else if(this.players.isEmpty) {
+    else if (this.players.isEmpty) {
       "Draw"
     }
     else {
@@ -48,15 +49,15 @@ class World(val boundariesSet: List[Boundary]) {
     }
   }
 
-//  def playersTakeDamage(): Unit = {
-//    for (player <- this.players) {
-//
-//    }
-//  }
+  //  def playersTakeDamage(): Unit = {
+  //    for (player <- this.players) {
+  //
+  //    }
+  //  }
 
-//  def playerMoves(player: Player): Unit = {
-//
-//  }
+  //  def playerMoves(player: Player): Unit = {
+  //
+  //  }
 
   def createPlayerBoundaries(): List[Boundary] = {
     var boundaryList: List[Boundary] = List.empty
@@ -66,8 +67,8 @@ class World(val boundariesSet: List[Boundary]) {
     boundaryList
   }
 
-//Luke - I'm trying to work on updating each of the "objects" (meaning players and projectiles) with new locations and hit detection
-// this is not finished by any means
+  //Luke - I'm trying to work on updating each of the "objects" (meaning players and projectiles) with new locations and hit detection
+  // this is not finished by any means
 
   def update(deltaTime: Double): Unit = {
     var playerBoundaries: List[Boundary] = createPlayerBoundaries()
@@ -75,50 +76,7 @@ class World(val boundariesSet: List[Boundary]) {
       player.update(deltaTime)
       val newPotentialLocation = player.computePotentialLocation(deltaTime)
       var collision = player.detectCollision(newPotentialLocation, this.boundariesSet)
-      if (collision == true){
-        player.updateVelocity(this, deltaTime)
-        player.location = newPotentialLocation
-        player.bottom = new PhysicsVector(player.location.x, player.location.y)
-        player.top = new PhysicsVector(player.location.x, player.location.y + 100)
-      }
-      else {
-        player.velocity.y = 0
-      }
-    }
-    for (projectile <- this.projectiles) {
-      val newProjectileLocation = projectile.computePotentialLocation(deltaTime)
-
-//      This will give the projectile bullet-drop
-      projectile.updateVelocity(this, deltaTime)
-
-      val projectileWallCollision = projectile.detectCollision(newProjectileLocation, this.boundariesSet)
-      if (projectileWallCollision == true) {
-        projectile.location = newProjectileLocation
-      }
-      if (projectileWallCollision == false) {
-//        var projectileBuffer = this.projectiles.toBuffer
-        this.projectiles - projectile
-//        this.projectiles = projectileBuffer.toList
-      }
-      for (player <- this.players) {
-        val projectilePlayerCollision = projectile.detectPlayerCollision(newProjectileLocation, player)
-        if (!projectilePlayerCollision.isEmpty) {
-          projectilePlayerCollision.head.health - 2
-        }
-      }
-    }
-    eliminatePlayers()
-  }
-
-  //This is a WIP for now, ignore if necessary
-
-  def singlePlayerUpdate(deltaTime: Double): Unit = {
-    var playerBoundaries: List[Boundary] = createPlayerBoundaries()
-    for (player <- this.players) {
-      player.update(deltaTime)
-      val newPotentialLocation = player.computePotentialLocation(deltaTime)
-      var collision = player.detectCollision(newPotentialLocation, this.boundariesSet)
-      if (collision == true){
+      if (collision == true) {
         player.updateVelocity(this, deltaTime)
         player.location = newPotentialLocation
         player.bottom = new PhysicsVector(player.location.x, player.location.y)
@@ -152,4 +110,43 @@ class World(val boundariesSet: List[Boundary]) {
     }
     eliminatePlayers()
   }
+
+  //This is a WIP for now, ignore if necessary
+
+  def singlePlayerUpdate(deltaTime: Double): Unit = {
+    var playerBoundaries: List[Boundary] = createPlayerBoundaries()
+    player.update(deltaTime)
+    val newPotentialLocation = player.computePotentialLocation(deltaTime)
+    var collision = player.detectCollision(newPotentialLocation, this.boundariesSet)
+    if (collision == true) {
+      player.updateVelocity(this, deltaTime)
+      player.location = newPotentialLocation
+      player.bottom = new PhysicsVector(player.location.x, player.location.y)
+      player.top = new PhysicsVector(player.location.x, player.location.y + 100)
+    }
+    else {
+      player.velocity.y = 0
+    }
+    for (projectile <- this.projectiles) {
+      val newProjectileLocation = projectile.computePotentialLocation(deltaTime)
+      //      This will give the projectile bullet-drop
+      projectile.updateVelocity(this, deltaTime)
+
+      val projectileWallCollision = projectile.detectCollision(newProjectileLocation, this.boundariesSet)
+      if (projectileWallCollision == true) {
+        projectile.location = newProjectileLocation
+      }
+      if (projectileWallCollision == false) {
+        //        var projectileBuffer = this.projectiles.toBuffer
+        this.projectiles - projectile
+        //        this.projectiles = projectileBuffer.toList
+      }
+
+      val projectilePlayerCollision = projectile.detectPlayerCollision(newProjectileLocation, player)
+      if (!projectilePlayerCollision.isEmpty) {
+        projectilePlayerCollision.head.health - 2
+      }
+    }
+  }
+  eliminatePlayers()
 }
