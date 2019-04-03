3bc27b7b7f02c386ad770ff316e27822e2006fd0
bmanaswini <manaswin@buffalo.edu>
2019-04-03 19:37:05 -0400

    Frontend

--- src/view/Game1.scala
+++ src/view/Game1.scala
@@ -1,6 +1,6 @@
 package view
 
-import controller.PressMovement
+import controller.{PressMovement}
 import javafx.scene.input.KeyEvent
 import main.Player
 import physics.{Boundary, PhysicsVector, World}
@@ -22,7 +22,7 @@ object Game1 extends JFXApp {
   val windowWidth: Double = 30 * scale_f
   val windowHeight: Double = 30 * scale_f
   var sceneGraphics: Group = new Group {}
-  var boundaries: List[Boundary] = List(new Boundary(new PhysicsVector(0,2000), new PhysicsVector(2000,2000)))
+  var boundaries: List[Boundary] = List(new Boundary(new PhysicsVector(0, 2000), new PhysicsVector(2000, 2000)))
   var game = new World(boundaries)
   var player = new Player(new PhysicsVector(200, 100), new PhysicsVector(0, 0), "abc", game)
   val playerSprite: Shape = playerBody(player.location.x, player.location.y, Color.Blue)
@@ -30,13 +30,7 @@ object Game1 extends JFXApp {
   game.players += player
   val players = game.players
   val six = 6
-  val bulletSprite: Shape = bulletBody(game.players.head.location.x,game.players.head.location.y,Color.Black)
 
-  sceneGraphics.children.add(playerSprite)
-
-  def computeDistance(v1: PhysicsVector, v2: PhysicsVector): Double = {
-    Math.sqrt(Math.pow(v1.x - v2.x, 2.0) + Math.pow(v1.y - v2.y, 2.0))
-  }
 
 
 
@@ -49,6 +43,7 @@ object Game1 extends JFXApp {
       fill = color
     }
   }
+
   def bulletBody(xLocation: Double, yLocation: Double, color: Color): Shape = {
     new Circle {
       centerX = xLocation
@@ -83,15 +78,10 @@ object Game1 extends JFXApp {
 
       rect.fill = Color.Green
       content = List(rect, obs1, obs2, obs4, obs5, obs6, obs7, obs8, obs9, obs11, obs12, obs13, obs14, obs15, obs17, obs20, obs21, obs22, playerSprite)
+
       addEventHandler(KeyEvent.KEY_PRESSED, new PressMovement(player))
       addEventHandler(KeyEvent.KEY_RELEASED, new PressMovement(player))
-      addEventHandler(KeyEvent.KEY_PRESSED, new PressMovement(player))
 
-
-      def bulletf(): Unit = {
-        content = List(bulletSprite)
-        
-      }
     }
     val update: Long => Unit = (time: Long) => {
       val dt: Double = (time - lastUpdateTime) / 1000000000.0
@@ -100,21 +90,44 @@ object Game1 extends JFXApp {
 
       playerSprite.translateX.value = game.players.head.location.x
       playerSprite.translateY.value = game.players.head.location.y
-
       if (game.projectiles.isEmpty) {
 
       }
       else {
-        bulletSprite.translateX.value = game.projectiles.head.location.x
-        bulletSprite.translateY.value = game.projectiles.head.location.y
+
+        scene = new Scene(2000, 2000) {
+          val bulletSprite: Shape = bulletBody(game.projectiles.head.location.x, game.projectiles.head.location.y, Color.Black)
+
+          var rect = Rectangle(5, 590, 2000, 200)
+          var obs1 = Rectangle(10, 200, 300, 10)
+          var obs2 = Rectangle(50, 300, 200, 10)
+          var obs4 = Rectangle(450, 256, 270, 10)
+          var obs5 = Rectangle(650, 440, 300, 10)
+          var obs6 = Rectangle(700, 200, 300, 10)
+          var obs7 = Rectangle(850, 150, 270, 10)
+          var obs8 = Rectangle(456, 350, 270, 10)
+          var obs9 = Rectangle(850, 300, 270, 10)
+          var obs11 = Rectangle(1050, 185, 270, 10)
+          var obs12 = Rectangle(20, 404, 270, 10)
+          var obs13 = Rectangle(1200, 300, 370, 10)
+          var obs14 = Rectangle(1100, 75, 350, 10)
+          var obs15 = Rectangle(5, 54, 300, 10)
+          var obs17 = Rectangle(1200, 450, 270, 10)
+          var obs20 = Rectangle(50, 500, 270, 10)
+          var obs21 = Rectangle(545, 50, 270, 10)
+          var obs22 = Rectangle(1050, 380, 270, 10)
+          rect.fill = Color.Green
+
+          content = List(rect, obs1, obs2, obs4, obs5, obs6, obs7, obs8, obs9, obs11, obs12, obs13, obs14, obs15, obs17, obs20, obs21, obs22, playerSprite, bulletSprite)
+        }
+
       }
-
-//      playerSprite.translateY.value = 10
-//      playerSprite.translateX.value = 10
-
     }
 
     AnimationTimer(update).start()
 
+
   }
 }
+
+
