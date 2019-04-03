41846bc81095685111377478ac658a55624c1cca
bmanaswini <manaswin@buffalo.edu>
2019-04-03 00:39:56 -0400

    Game frontend

--- src/view/Game1.scala
+++ src/view/Game1.scala
@@ -3,13 +3,14 @@ package view
 import controller.PressMovement
 import javafx.scene.input.KeyEvent
 import main.Player
+import objects.Projectile
 import physics.{Boundary, PhysicsVector, World}
 import scalafx.animation.AnimationTimer
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.canvas.Canvas
 import scalafx.scene.paint.Color
-import scalafx.scene.shape.{Rectangle, Shape}
+import scalafx.scene.shape.{Circle, Rectangle, Shape}
 import scalafx.scene.{Group, Scene}
 
 import scala.collection.mutable
@@ -22,38 +23,50 @@ object Game1 extends JFXApp {
   val windowWidth: Double = 30 * scale_f
   val windowHeight: Double = 30 * scale_f
   var sceneGraphics: Group = new Group {}
-  var boundaries: List[Boundary] = List(new Boundary(new PhysicsVector(0,2000), new PhysicsVector(2000,2000)))
+  var boundaries: List[Boundary] = List(new Boundary(new PhysicsVector(5,90), new PhysicsVector(2000,200)))
   var game = new World(boundaries)
-  var player = new Player(new PhysicsVector(200, 100), new PhysicsVector(0, 0), "abc", game)
-  val playerSprite: Shape = playerSprite(player.location.x, player.location.y, Color.Blue)
+  var player = new Player(new PhysicsVector(0, 1), new PhysicsVector(1, 1), "abc", game)
   var playerBuffer: mutable.ListBuffer[Player] = ListBuffer[Player](player)
   game.players += player
-  val players = game.players
+  var players = game.players
   val six = 6
+  val playerBody: Shape = playerBody(game.players.head.location.x, game.players.head.location.y, Color.Blue)
+val bulletBody:Shape=bulletBody(game.players.head.location.x,game.players.head.location.y,Color.Black)
+  var bullet:Projectile=new Projectile(game.players.head.location,game.players.head.velocity)
+//  sceneGraphics.children.add(playerSprite)
+//
+//  def computeDistance(v1: PhysicsVector, v2: PhysicsVector): Double = {
+//    Math.sqrt(Math.pow(v1.x - v2.x, 2.0) + Math.pow(v1.y - v2.y, 2.0))
+//  }
 
-  sceneGraphics.children.add(playerSprite)
+//  def convertX(gameX: Double): Double = {
+//    gameX* 10
+//  }
+//
+//  def convertY(gameY: Double): Double = {
+//    gameY  * 10
+//  }
 
-  def computeDistance(v1: PhysicsVector, v2: PhysicsVector): Double = {
-    Math.sqrt(Math.pow(v1.x - v2.x, 2.0) + Math.pow(v1.y - v2.y, 2.0))
-  }
-
-  def convertX(gameX: Double, width: Double): Double = {
-    gameX* scale_f
-  }
-
-  def convertY(gameY: Double, height: Double): Double = {
-    gameY  * scale_f
-  }
-
-  def playerSprite(xLocation: Double, yLocation: Double, color: Color): Shape = {
+  def playerBody(xLocation: Double, yLocation: Double, color: Color): Shape = {
     new Rectangle {
       width = 3 * scale_f
       height = 5 * scale_f
-      translateX = convertX(xLocation,10)
-      translateY = convertY(yLocation,0)
+      translateX = xLocation*10
+      translateY = yLocation*10
       fill = color
     }
   }
+  def bulletBody(xLocation: Double, yLocation: Double, color: Color): Shape = {
+     new Circle {
+      centerX = xLocation
+      centerY = yLocation
+      radius = 10.0
+      fill = Color.Green
+    }
+  }
+
+
+
 
   this.stage = new PrimaryStage {
     this.title = "Battle World"
@@ -79,18 +92,18 @@ object Game1 extends JFXApp {
       var obs22 = Rectangle(1050, 380, 270, 10)
       obs1.fill = Color.Red
       rect.fill = Color.Green
-      content = List(rect, obs1, obs2, obs4, obs5, obs6, obs7, obs8, obs9, obs11, obs12, obs13, obs14, obs15, obs17, obs20, obs21, obs22, playerSprite)
-      addEventHandler(KeyEvent.KEY_PRESSED, new PressMovement(player))
-      addEventHandler(KeyEvent.KEY_RELEASED, new PressMovement(player))
+      content = List(rect, obs1, obs2, obs4, obs5, obs6, obs7, obs8, obs9, obs11, obs12, obs13, obs14, obs15, obs17, obs20, obs21, obs22, playerBody)
+
+      addEventHandler(KeyEvent.KEY_PRESSED, new PressMovement(game.players.head))
+      addEventHandler(KeyEvent.KEY_RELEASED, new PressMovement(game.players.head))
 
     }
     val update: Long => Unit = (time: Long) => {
       val dt: Double = (time - lastUpdateTime) / 1000000000.0
       lastUpdateTime = time
       game.update(dt)
-
-      playerSprite.translateX.value = game.players.head.location.x
-      playerSprite.translateY.value = game.players.head.location.y
+      playerBody.translateX.value = game.players.head.location.x*10
+      playerBody.translateY.value = game.players.head.location.y*10
 //      playerSprite.translateY.value = 10
 //      playerSprite.translateX.value = 10
 
